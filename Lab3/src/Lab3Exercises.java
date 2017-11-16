d21d9e6f94e52d577540950eee5a5d912577f6c2
alina.gonzalez <alina.gonzalez@alinas-mbp.zyxel.com>
2017-11-16 17:02:10 -0400

    Added lines and an oval.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,13 @@
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame("Alina");
+        //myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        myFrame.setLocation(917, 374);
+        myFrame.setSize(200, 200);
+        MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
+        myFrame.setVisible(true);
+    }
 }
